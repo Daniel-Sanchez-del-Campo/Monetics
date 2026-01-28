2e8c8930a1905dc6d1368c5b7fe61346cd07efaf
Daniel Sánchez del Campo <daniel.sanchez.delcampo2003@gmail.com>
2026-01-28 14:00:07 +0100

    inicio

---
+++ Monetics/MoneticsBack/src/test/java/com/monetics/moneticsback/MoneticsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monetics.moneticsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneticsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
