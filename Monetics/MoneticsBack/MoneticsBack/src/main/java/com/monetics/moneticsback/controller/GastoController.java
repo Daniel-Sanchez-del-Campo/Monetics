3982faf6e75ef1121e9f151adfe196efd1de3343
Beatriz Siguero Sánchez-Crespo <beatriz.siguero_sanchez-crespo@estudiante.xtart.com>
2026-02-03 13:44:35 +0100

    comentarioAdd

--- Monetics/MoneticsBack/MoneticsBack/src/main/java/com/monetics/moneticsback/controller/GastoController.java
+++ Monetics/MoneticsBack/MoneticsBack/src/main/java/com/monetics/moneticsback/controller/GastoController.java
@@ -8,7 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/**
+ * Controller REST encargado de los endpoints de gastos.
+ *
+ * Este controller:
+ * - No contiene lógica de negocio
+ * - Trabaja exclusivamente con DTOs
+ * - Delegada todo en el service
+ */
 
 @RestController
 @RequestMapping("/api/gastos")
